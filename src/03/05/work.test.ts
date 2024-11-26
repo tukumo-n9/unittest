f5e97af593f81db1cad30eb84405d1459da25394
tukumo-n9 <tukumon9.work@gmail.com>
2024-11-26 22:13:52 +0900

    3-5

--- src/03/05/work.test.ts
+++ src/03/05/work.test.ts
@@ -1,4 +1,4 @@
-import { add, RangeError, sub } from "./index";
+import { add, RangeError, sub } from "./work";
 
 describe("四則演算", () => {
   describe("add", () => {
@@ -8,8 +8,8 @@ describe("四則演算", () => {
     test("合計の上限は、'100'である", () => {
       expect(add(70, 80)).toBe(100);
     });
-    test("引数が'0〜100'の範囲外だった場合、例外をスローする", () => {
-      const message = "入力値は0〜100の間で入力してください";
+    test("引数が'0~100'の範囲外だった場合、例外をスローする", () => {
+      const message = "入力値は0~100の間で入力してください";
       expect(() => add(-10, 10)).toThrow(message);
       expect(() => add(10, -10)).toThrow(message);
       expect(() => add(-10, 110)).toThrow(message);
@@ -22,10 +22,10 @@ describe("四則演算", () => {
     test("返り値の合計は、下限が'0'である", () => {
       expect(sub(70, 80)).toBe(0);
     });
-    test("引数が'0〜100'の範囲外だった場合、例外をスローする", () => {
-      expect(() => sub(-10, 10)).toThrow(RangeError);
-      expect(() => sub(10, -10)).toThrow(RangeError);
-      expect(() => sub(-10, 110)).toThrow(Error);
+    test("引数が'0~100'の範囲外だった場合、例外をスローする", () => {
+      expect(() => add(-10, 10)).toThrow(RangeError);
+      expect(() => add(10, -10)).toThrow(RangeError);
+      expect(() => add(-10, 110)).toThrow(Error);
     });
   });
 });
